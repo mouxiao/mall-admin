ff57412a1d3aaa7aa03d5deb67ee2e54115b6503
mouxiao <mouxiao1014@gmail.com>
2018-11-07 17:27:32 +0800

    init

---
+++ c-mall-commons/src/main/java/com/mall/commons/CMallCommonsApplication.java
@@ -0,0 +1,12 @@
+package com.mall.commons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CMallCommonsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CMallCommonsApplication.class, args);
+	}
+}
---
+++ c-mall-platform/src/main/java/com/mall/platform/CMallPlatformApplication.java
@@ -0,0 +1,14 @@
+package com.mall.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = "com.mall")
+public class CMallPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CMallPlatformApplication.class, args);
+	}
+}
